b9698b435c7a1a1a94970d9c30852f5de447b4c2
cchrispy <cchiang1992@gmail.com>
2016-08-10 20:33:25 -0700

    Completed songModel, songQueue, songQueueView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function(model) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+    this.on('ended', function(model) {
+      this.remove(this.at(0));
+    });
+    this.on('remove', function(model, collection, options) {
+      if (this.length) {
+        this.playFirst();
+      }
+    });
+    this.on('dequeue', function(model) {
+      this.remove(model);
+    });
+  },
+  playFirst: function() {
+    this.at(0).play();
+  },
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,15 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this); 
   },
 
   render: function() {
+    this.collection.forEach(function(song) {
+      var view = new SongQueueEntryView({model: song});
+      view.render();
+    });
     return this.$el;
   }
 
